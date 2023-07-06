b178fbe1c55eaea5a881799f8d1977bbde012fe1
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-06 09:56:30 +0700

    Add start files

---
+++ PalmCardsApp.swift
@@ -0,0 +1,23 @@
+//
+//  PalmCardsApp.swift
+//  PalmCards
+//
+//  Created by Вадим Мартыненко on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PalmCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
