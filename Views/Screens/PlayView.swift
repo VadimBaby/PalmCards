29b5cab095b78fa2962c8ce16487e5aee14e361f
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-13 16:17:04 +0700

    Create UI for PlayView

--- Views/Screens/PlayView.swift
+++ Views/Screens/PlayView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct PlayView: View {
+    @EnvironmentObject var dictionaryViewModel: DictionaryViewModel
+    @EnvironmentObject var settings: Settings
+    @State private var doCardsNavigate: Bool = false
+    @State private var doWritingNavigate: Bool = false
+    @State var chosenDictionaries: [String] = []
+
+    let listOfTypeGames = ["cards", "write words"]
+    
     var body: some View {
-        ZStack {
-            BackgroundColor()
-            
-            Text("PlayView")
+        NavigationStack{
+            ZStack {
+                BackgroundColor()
+                
+                VStack{
+                    if dictionaryViewModel.listDictionaries.isEmpty {
+                        NoItemDictionaryView()
+                    } else {
+                        VStack{
+                            List {
+                                ForEach(dictionaryViewModel.listDictionaries) { dictionary in
+                                    Text(dictionary.name)
+                                }
+                            }
+                            .frame(height: 400)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Text("Играть")
+                            .tint(Color.white)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 55)
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                            .padding()
+                    }
+                }
+            }
+            .navigationTitle("Играть")
         }
     }
 }
@@ -20,5 +56,7 @@ struct PlayView: View {
 struct PlayView_Previews: PreviewProvider {
     static var previews: some View {
         PlayView()
+            .environmentObject(DictionaryViewModel())
+            .environmentObject(Settings())
     }
 }
