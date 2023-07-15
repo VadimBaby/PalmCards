181cee9d14b9f46e27b86a6144cb1e2806f3708a
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-15 17:55:38 +0700

    WritePlayingView

---
+++ Views/Screens/WritePlayingView.swift
@@ -0,0 +1,20 @@
+//
+//  WritePlayingView.swift
+//  PalmCards
+//
+//  Created by Вадим Мартыненко on 15.07.2023.
+//
+
+import SwiftUI
+
+struct WritePlayingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WritePlayingView_Previews: PreviewProvider {
+    static var previews: some View {
+        WritePlayingView()
+    }
+}
