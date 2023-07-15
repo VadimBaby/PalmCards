80582b0db6cec1f53eecced6c42342dd720c9b70
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-15 19:39:32 +0700

    Created basic variables in CardsGameLogic

--- CardsGameLogic.swift
+++ CardsGameLogic.swift
@@ -9,4 +9,24 @@ import Foundation
 
 class CardsGameLogic: ObservableObject {
     
+    @Published var shuffleWords: [WordModel] = []
+    @Published var countWords: Int = 0
+    @Published var indexShuffleWords: Int = 0
+    @Published var title: String = ""
+    @Published var upTitle: String = ""
+    @Published var downTitle: String = ""
+    @Published var countRightAnswers: Int = 0
+    @Published var countWrongAnswers: Int = 0
+    @Published var showResults: Bool = false
+    @Published var firstShowTranslate: Bool = false
+    @Published var hideExample: Bool = false
+    
+    var lastIndexElement = 0
+    
+    enum GuessMoment {
+        case rotate
+        case select
+    }
+    
+    @Published var guessMoment: GuessMoment = .rotate
 }
