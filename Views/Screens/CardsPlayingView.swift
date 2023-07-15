3335d2deb4bd03f4a19c7b503115df201ad8f7b5
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-15 17:54:28 +0700

    Create CardsPlayingView

---
+++ Views/Screens/CardsPlayingView.swift
@@ -0,0 +1,20 @@
+//
+//  CardsPlayingView.swift
+//  PalmCards
+//
+//  Created by Вадим Мартыненко on 15.07.2023.
+//
+
+import SwiftUI
+
+struct CardsPlayingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardsPlayingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsPlayingView()
+    }
+}
