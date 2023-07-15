87884d052ef17f86f7c94e440e1f6d708e8d12da
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-15 18:09:27 +0700

    Add DictionaryViewModel and Settings in CardsPlayingView

--- Views/Screens/CardsPlayingView.swift
+++ Views/Screens/CardsPlayingView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CardsPlayingView: View {
+    
+    @EnvironmentObject var dictionaryViewModel: DictionaryViewModel
+    @EnvironmentObject var settings: Settings
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -16,5 +20,7 @@ struct CardsPlayingView: View {
 struct CardsPlayingView_Previews: PreviewProvider {
     static var previews: some View {
         CardsPlayingView()
+            .environmentObject(DictionaryViewModel())
+            .environmentObject(Settings())
     }
 }
