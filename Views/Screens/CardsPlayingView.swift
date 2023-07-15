e95a8440b7a56f6eed72e20d107804e034bcf01d
Vadim Martynenko <vadimcool9358@gmail.com>
2023-07-15 18:19:26 +0700

    Add UI to CardsPlayingView

--- Views/Screens/CardsPlayingView.swift
+++ Views/Screens/CardsPlayingView.swift
@@ -12,8 +12,108 @@ struct CardsPlayingView: View {
     @EnvironmentObject var dictionaryViewModel: DictionaryViewModel
     @EnvironmentObject var settings: Settings
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            BackgroundColor()
+            
+            if true {
+                GameView
+            } else {
+                VStack{}
+            }
+            
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+    
+    var GameView: some View {
+        VStack{
+            HStack{
+                Button(action: {}, label: {
+                    Text("Завершить")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .padding(10)
+                        .background(colorScheme == .dark ? Color.white.opacity(0.2) : Color.black)
+                        .cornerRadius(5)
+                })
+                Spacer()
+                Text("0")
+            }
+            .padding()
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 15)
+                .fill(colorScheme == .dark ? Color.white.opacity(0.2) : Color.white)
+                .overlay(
+                    VStack{
+                        Text("showUpTitle")
+                        Spacer()
+                        Text("title")
+                            .font(.title)
+                        Spacer()
+                        Text("donwTitle")
+                    }
+                        .padding(.vertical)
+                )
+                .frame(maxWidth: .infinity)
+                .frame(height: 300)
+                .padding(.horizontal)
+                .rotation3DEffect(.degrees(0), axis: (0, 1, 0))
+            
+            Spacer()
+            if true {
+                nextButton
+            } else {
+                xmarkAndCheckmarkButtons
+            }
+        }
+    }
+    
+    // buttons
+    
+    var nextButton: some View {
+        HStack{
+            Button(action: {}, label: {
+                Text("Повернуть")
+                    .foregroundColor(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(false ? Color.gray.opacity(0.2) : Color.blue)
+                    .cornerRadius(5)
+            })
+            .disabled(false)
+        }
+        .padding()
+    }
+    
+    var xmarkAndCheckmarkButtons: some View {
+        HStack(spacing: 50){
+            Button(action: {}, label: {
+                Image(systemName: "xmark")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 30)
+                    .frame(height: 55)
+                    .background(false ? Color.gray.opacity(0.2) : Color.red)
+                    .cornerRadius(10)
+            })
+            .disabled(false)
+            
+            Button(action: {}, label: {
+                Image(systemName: "checkmark")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                    .padding(.horizontal, 30)
+                    .frame(height: 55)
+                    .background(false ? Color.gray.opacity(0.2) : Color.green)
+                    .cornerRadius(10)
+            })
+            .disabled(false)
+        }
+        .padding()
     }
 }
 
