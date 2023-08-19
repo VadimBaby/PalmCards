539d9c3210da255f32be7b51b307f51e20d2197c
VadimBaby <vadimdim22@gmail.com>
2023-08-19 10:52:39 +0700

    Fixed test code in func getNextRemindNotificationInHours in RemindNotificationManager

--- RemindNotificationManager.swift
+++ RemindNotificationManager.swift
@@ -110,25 +110,7 @@ class RemindNotificationManager: ObservableObject {
         
         switch lastRemindInHours {
         
-//        case 0:
-//            return 6
-            
         case 0:
-            return 1
-        
-        case 1:
-            return 2
-        
-        case 2:
-            return 3
-        
-        case 3:
-            return 4
-        
-        case 4:
-            return 5
-        
-        case 5:
             return 6
             
         case 6:
